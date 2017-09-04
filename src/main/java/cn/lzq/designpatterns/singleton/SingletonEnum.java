37c85bbc3cb17d771f98dddaf8561cdc5c3684e9
lianzq <254663475@qq.com>
2017-09-04 17:02:50 +0800

    singleton

---
+++ src/main/java/cn/lzq/designpatterns/singleton/SingletonEnum.java
@@ -0,0 +1,14 @@
+package cn.lzq.designpatterns.singleton;
+
+/**
+ * 枚举方式
+ *
+ * @author lianzhiqiang
+ * Created in 2017/9/4
+ */
+public enum SingletonEnum {
+    INSTANCE;
+
+    public void otherMethod() {
+    }
+}
