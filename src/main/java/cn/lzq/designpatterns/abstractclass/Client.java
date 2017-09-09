f7a327d2f408af95e567618cea01eabf58bb69b6
lianzq <254663475@qq.com>
2017-09-09 14:30:53 +0800

    abstract class

---
+++ src/main/java/cn/lzq/designpatterns/abstractclass/Client.java
@@ -0,0 +1,15 @@
+package cn.lzq.designpatterns.abstractclass;
+
+/**
+ * @author lianzhiqiang
+ * Created in 2017/9/9
+ */
+public class Client {
+    public static void main(String[] args) {
+        AbstractClass abstractClass1 = new ConcreteClass1();
+        AbstractClass abstractClass2 = new ConcreteClass2();
+        //调用模板方法
+        abstractClass1.templateMethod();
+        abstractClass2.templateMethod();
+    }
+}
