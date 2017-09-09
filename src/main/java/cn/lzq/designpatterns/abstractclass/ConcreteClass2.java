eb20755341b562cdec06421af4cb7e442e9210a8
lianzq <254663475@qq.com>
2017-09-09 14:30:42 +0800

    abstract class

---
+++ src/main/java/cn/lzq/designpatterns/abstractclass/ConcreteClass2.java
@@ -0,0 +1,18 @@
+package cn.lzq.designpatterns.abstractclass;
+
+/**
+ * @author lianzhiqiang
+ * Created in 2017/9/9
+ */
+public class ConcreteClass2 extends AbstractClass {
+
+    //实现基本方法
+    protected void doSomething() {
+
+    }
+
+    //实现基本方法
+    protected void doAnything() {
+
+    }
+}
