e7cbddf49d05d50089bc894c36df0cc0ebf41069
lianzq <254663475@qq.com>
2017-09-13 18:02:06 +0800

    Proxy

---
+++ src/main/java/cn/lzq/designpatterns/proxy/Proxy.java
@@ -0,0 +1,37 @@
+package cn.lzq.designpatterns.proxy;
+
+/**
+ * @author lianzhiqiang
+ * Created in 2017/9/13
+ */
+public class Proxy implements Subject {
+
+    private Subject subject = null;
+
+    public Proxy() {
+        this.subject = new Proxy();
+    }
+
+    public Proxy(Object... objects) {
+    }
+
+    public Proxy(Object _subject) {
+        this.subject = (Subject) _subject;
+    }
+
+    @Override
+    public void request() {
+        this.before();
+        this.subject.request();
+        this.after();
+    }
+
+    //预处理
+    private void before() {
+        //doSomething
+    }
+
+    //善后处理
+    private void after() {
+    }
+}
