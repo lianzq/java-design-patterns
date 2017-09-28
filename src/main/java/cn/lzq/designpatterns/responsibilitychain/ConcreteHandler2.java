191d2c2d52ff0f87ec29d53b2922b8421c3002ae
lianzq <254663475@qq.com>
2017-09-28 14:44:34 +0800

    chain

---
+++ src/main/java/cn/lzq/designpatterns/responsibilitychain/ConcreteHandler2.java
@@ -0,0 +1,18 @@
+package cn.lzq.designpatterns.responsibilitychain;
+
+/**
+ * @author lianzhiqiang
+ * Created in 2017/9/28
+ */
+public class ConcreteHandler2 extends Handler {
+
+    @Override
+    protected Level getHandlerLevel() {
+        return null;
+    }
+
+    @Override
+    protected Response echo(Request request) {
+        return null;
+    }
+}
