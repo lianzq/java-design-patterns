9ffdc25c9df536206795f26d844899861337d3ff
lianzq <254663475@qq.com>
2017-09-28 14:44:50 +0800

    chain

---
+++ src/main/java/cn/lzq/designpatterns/responsibilitychain/Client.java
@@ -0,0 +1,23 @@
+package cn.lzq.designpatterns.responsibilitychain;
+
+/**
+ * @author lianzhiqiang
+ * Created in 2017/9/28
+ */
+public class Client {
+
+    public static void main(String[] args) {
+        //声明所以处理节点
+        Handler handler1 = new ConcreteHandler1();
+        Handler handler2 = new ConcreteHandler1();
+        Handler handler3 = new ConcreteHandler1();
+
+        //设定链中的阶段书序1--》2--》3
+        handler1.setNext(handler2);
+        handler2.setNext(handler3);
+
+        //提交请求，返回结果
+        handler1.handlerMessage(new Request());
+
+    }
+}
