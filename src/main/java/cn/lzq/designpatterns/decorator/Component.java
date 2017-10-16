4e7381c71587fdedf01c9875970cea6fb6274e9d
lianzq <254663475@qq.com>
2017-10-16 20:03:32 +0800

    Decorator

---
+++ src/main/java/cn/lzq/designpatterns/decorator/Component.java
@@ -0,0 +1,10 @@
+package cn.lzq.designpatterns.decorator;
+
+/**
+ * @author lianzhiqiang
+ * Created in 2017/10/16
+ */
+public abstract class Component {
+    
+    public abstract void operate();
+}
