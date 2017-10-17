9edc213ba27fd6ea1bdf256aea20d2c6ba4c3d28
lianzq <254663475@qq.com>
2017-10-17 16:34:33 +0800

    Strategy

---
+++ src/main/java/cn/lzq/designpatterns/strategy/Strategy.java
@@ -0,0 +1,13 @@
+package cn.lzq.designpatterns.strategy;
+
+/**
+ * @author lianzhiqiang
+ * Created in 2017/10/17
+ */
+public interface Strategy {
+
+    /**
+     * 策略模式的运算法则
+     */
+    void doSomething();
+}
