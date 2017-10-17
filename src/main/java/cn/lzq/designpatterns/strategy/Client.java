15113f617e7d15c1d67531ad0bf4eb83dacbb0fc
lianzq <254663475@qq.com>
2017-10-17 16:36:27 +0800

    Strategy

---
+++ src/main/java/cn/lzq/designpatterns/strategy/Client.java
@@ -0,0 +1,19 @@
+package cn.lzq.designpatterns.strategy;
+
+/**
+ * @author lianzhiqiang
+ * Created in 2017/10/17
+ */
+public class Client {
+    public static void main(String[] args) {
+        //声明一个具体的策略
+        Strategy strategy = new ConcreteStrategy1();
+        //声明上下文对象
+        Context context = new Context(strategy);
+        //执行封装后的方法
+        context.doAnything();
+
+        context = new Context(new ConcreteStrategy2());
+        context.doAnything();
+    }
+}
