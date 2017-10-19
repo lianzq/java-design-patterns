5e1b396a7beef10b7343188a6d4f18877632cbd1
lianzq <254663475@qq.com>
2017-10-19 16:20:20 +0800

    Adapter

---
+++ src/main/java/cn/lzq/designpatterns/adapter/Adaptee.java
@@ -0,0 +1,12 @@
+package cn.lzq.designpatterns.adapter;
+
+/**
+ * @author lianzhiqiang
+ * Created in 2017/10/19
+ */
+public class Adaptee {
+
+    public void doSomething() {
+        System.out.println(" 我很忙，请让我好好工作！");
+    }
+}
